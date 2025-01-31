ed9fdd0fc45ce9531c41dd224c2a420c93fd7801
yicong-jiang <128440033+yicong-jiang@users.noreply.github.com>
2025-01-31 00:04:10 -0500

    Update data_generate.py

--- data_generate.py
+++ data_generate.py
@@ -46,7 +46,7 @@ def gen_ols_data(
     )
 
 
-# generate ols data for Section 4.1.1
+# generate OLS data for Section 4.1.1
 def gen_ols_data_high(
     n = 2000,
     p = 50,
@@ -96,3 +96,61 @@ def gen_ols_data_high(
         beta = beta,
     )
 
+# generate Lognormal data for Section 4.1.2
+def gen_ols_data_lognorm(
+    n = 1000,
+    p = 50,
+    rho = 0,
+    tau = 0.3,
+    beta1 = 1,
+    beta2 = 1,
+    sigmae = 0.5,
+    sigma = 0.5,
+	dgp_seed=1,
+    W_p = 0.5,
+):
+    np.random.seed(dgp_seed)
+  
+    # sample beta
+    #beta_Y = np.random.randn(p) / np.sqrt(p) 
+    #beta_Z = np.random.randn(p) / np.sqrt(p)
+    beta = np.random.randn(p)
+    beta = beta / np.sqrt(np.sum(beta ** 2))
+
+    # sample X
+    #print(np.matrix(np.ones(p)).transpose() @ np.matrix(beta))
+    X = np.random.randn(n * p).reshape((n, p)) 
+
+
+    # sample Z
+    W_Z = np.random.randn(n)
+    t_z = 1
+    W_Z = W_Z  / np.sqrt(3) * np.abs(W_Z)
+    #W_Z = (np.exp(t_z * W_Z) - np.exp(0.5 * t_z * t_z)) / np.sqrt(np.exp(2 * t_z * t_z) - np.exp(t_z * t_z))
+
+    S = np.exp(-(X @ beta)  + sigma * W_Z)
+
+    
+    # sample Y
+    W_Y = np.random.randn(n)
+    t_y = 1
+    W_Y = W_Y / np.sqrt(3) * np.abs(W_Y)
+    #W_Y = (np.exp(t_y * W_Y) - np.exp(0.5 * t_y * t_y)) / np.sqrt(np.exp(2 * t_y * t_y) - np.exp(t_y * t_y))
+    #W_Y = (np.exp(W_Y) - np.exp(0.5)) / np.sqrt(np.exp(2) - np.exp(1))
+
+    Y = np.exp((X @ beta) + sigmae * W_Y)
+
+    
+    
+    #sample W
+    W = np.random.binomial(1, W_p * np.ones(n))
+
+    
+    return dict(
+        Y = Y, 
+        W = W,
+        X = X,
+        S = S,
+        W_Z = W_Z,
+        W_Y = W_Y,
+        beta = b
