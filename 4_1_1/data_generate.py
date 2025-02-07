9777f4f8591362c7dd0ad1d64d7ca9252c67ecb2
yicong-jiang <128440033+yicong-jiang@users.noreply.github.com>
2025-02-06 20:37:13 -0500

    Update data_generate.py

--- 4_1_1/data_generate.py
+++ 4_1_1/data_generate.py
@@ -1,7 +1,8 @@
 from tool/util import *
 
-def gen_ols_data(
+def gen_ols_data_high(
     n = 2000,
+    p = 50,
     rho = 0,
     tau = 0.3,
     beta1 = 1,
@@ -9,36 +10,51 @@ def gen_ols_data(
     sigmae = 0.5,
     sigma = 0.5,
 	dgp_seed=1,
+    W_p = 0.5,
 ):
     np.random.seed(dgp_seed)
-    # sample Z
-    Z1 = np.random.randn(n)
-    Z2 = np.random.randn(n)
-    Z2 = rho * Z1 + np.sqrt(1 - rho * rho) * Z2
-    S = (Z1 - rho * Z2) / (1 - rho * rho)
-    
-    # sample Wer
-    Wer1 = np.random.randn(n) * sigma
-    Wer2 = np.random.randn(n) * sigma
-    Wer2 = tau * Wer1 + np.sqrt(1 - tau * tau) * Wer2
-    
+  
+    # sample beta
+    #beta_Y = np.random.randn(p) / np.sqrt(p) 
+    #beta_Z = np.random.randn(p) / np.sqrt(p)
+    beta = np.random.randn(p)
+    beta = beta / np.sqrt(np.sum(beta ** 2))
+
     # sample X
-    X1 = Z1 + Wer1
-    X2 = Z2 + Wer2
-    X = np.vstack((X1, X2)).T
+    #print(np.matrix(np.ones(p)).transpose() @ np.matrix(beta))
+    X = 0.5 * np.random.randn(n * p).reshape((n, p)) 
+
+
+    # sample Z
+    W_Z = np.random.randn(n)
+    t_z = 1
+    #W_Z = W_Z ** 3 / np.sqrt(15) #* np.abs(W_Z)
+    #W_Z = (np.exp(t_z * W_Z) - np.exp(0.5 * t_z * t_z)) / np.sqrt(np.exp(2 * t_z * t_z) - np.exp(t_z * t_z))
+
+    S = np.sqrt(0.75) + (X @ beta)  + sigma * W_Z
+
     
     # sample Y
-    Y = beta1 * Z1 + beta2 * Z2 + np.random.randn(n) * sigmae
+    W_Y = np.random.randn(n)
+    t_y = 1
+    #W_Y = W_Y ** 3 / np.sqrt(15) #* np.abs(W_Y)
+    #W_Y = (np.exp(t_y * W_Y) - np.exp(0.5 * t_y * t_y)) / np.sqrt(np.exp(2 * t_y * t_y) - np.exp(t_y * t_y))
+    #W_Y = (np.exp(W_Y) - np.exp(0.5)) / np.sqrt(np.exp(2) - np.exp(1))
+
+    Y = np.sqrt(0.75) + (X @ beta) + sigmae * W_Y
+
     
     
     #sample W
-    W = np.random.binomial(1, 0.5 * np.ones(n))
+    W = np.random.binomial(1, W_p * np.ones(n))
+
     
     return dict(
         Y = Y, 
         W = W,
         X = X,
         S = S,
-        Z1 = Z1,
-        Z2 = Z2,
+        W_Z = W_Z,
+        W_Y = W_Y,
+        beta = beta,
     )
